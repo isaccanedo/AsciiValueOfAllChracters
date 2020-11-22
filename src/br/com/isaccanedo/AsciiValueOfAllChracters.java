59c27967a7bccde0bf9472a945d3ba8e5a220511
isaccanedo <isaccanedo@gmail.com>
2020-11-22 11:53:08 -0300

    :fish: Initial commit

---
+++ src/br/com/isaccanedo/AsciiValueOfAllChracters.java
@@ -0,0 +1,17 @@
+package br.com.isaccanedo;
+
+/**
+ * @author Isac Canedo
+ */
+
+public class AsciiValueOfAllChracters {
+    
+    public static void main(String[] args) {
+        for (int i = 0; i <=255 ; i++) {
+
+            System.out.println("O valor ASCII de "+(char)i+" = "+i);
+            
+        }
+    }
+    
+}
